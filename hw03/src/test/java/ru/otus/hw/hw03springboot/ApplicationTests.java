811a97080b94cde7c98d4956d94554cdd13e7acc
acid <van132011@gmail.com>
2024-02-28 23:43:33 +0300

    hw03 - run app with CommandLineRunner

--- hw03/src/test/java/ru/otus/hw/hw03springboot/ApplicationTests.java
+++ hw03/src/test/java/ru/otus/hw/hw03springboot/ApplicationTests.java
@@ -2,6 +2,10 @@ package ru.otus.hw.hw03springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootTest
 class ApplicationTests {
@@ -10,4 +14,9 @@ class ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Configuration
+	@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = TestRunnerService.class))
+	static class TestConfig {
+
+	}
 }
