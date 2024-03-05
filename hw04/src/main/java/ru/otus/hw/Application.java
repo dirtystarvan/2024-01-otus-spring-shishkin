b7fd4b159d1c4c811beb1e8e574568a67cbdb7c3
acid <van132011@gmail.com>
2024-03-05 21:23:06 +0300

    hw04 - redundant annotation

--- hw04/src/main/java/ru/otus/hw/Application.java
+++ hw04/src/main/java/ru/otus/hw/Application.java
@@ -1,11 +1,9 @@
 package ru.otus.hw;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
